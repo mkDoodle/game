e3cd6a1498b8d8c57204b57f480bcd072023e6e4
Chris <chrisdawson1996@gmail.com>
2016-08-24 10:43:45 +0100

    Added some compiler switches to build.bat

--- build.bat
+++ build.bat
@@ -3,6 +3,6 @@
 mkdir ..\..\build
 pushd ..\..\build
 
-cl  -WX -W4 -wd4201 -DGAME_SLOW=1 -DGAME_INTERNAL=1 -Zi -FC ..\game\code\win32_game.cpp gdi32.lib user32.lib
+cl -MT -nologo -EHa- -GR- -Od -Oi -WX -W4 -wd4201 -wd4100 -wd4189 -DGAME_SLOW=1 -DGAME_INTERNAL=1 -Zi -FC ..\game\code\win32_game.cpp /link -subsystem:windows,5.1 gdi32.lib user32.lib
 
 popd
